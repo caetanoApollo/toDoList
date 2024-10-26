a52209e8abab08d5716d50ec9fa48ec03b3bcbec
caetanoApollo <caetanosilveira1908@gmail.com>
2024-10-26 15:17:49 -0300

    Deploy

--- public/db_config.js
+++ public/db_config.js
@@ -1,28 +1,13 @@
+// db_config.js
 require('dotenv').config();
 const mysql = require('mysql');
-
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-});
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  max_connections: 10
+  connectionLimit: 10 // Define um limite para o número de conexões
 });
 
-db.connect((err) => {
-  if (err) {
-    console.error('Erro ao conectar ao banco de dados:', err);
-    return;
-  }
-  console.log('Conectado ao banco de dados MySQL!');
-});
-
-module.exports = db;
+module.exports = pool;
