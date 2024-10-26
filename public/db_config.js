c3b705e4e85770900189c817138a60c883133431
Apollo <109846305+caetanoApollo@users.noreply.github.com>
2024-10-26 14:40:06 -0300

    Update db_config.js

--- public/db_config.js
+++ public/db_config.js
@@ -1,12 +1,15 @@
-const mysql = require('mysql2');
+require('dotenv').config();
+const mysql = require('mysql');
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST, 
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306
+  port: process.env.DB_PORT || 3306,
 });
+
+module.exports = connection;
 
 db.connect((err) => {
     if (err) {
