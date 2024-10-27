59117d5cfe917e041dadb4b14bdde43bc01ca2b4
caetanoApollo <caetanosilveira1908@gmail.com>
2024-10-26 22:57:22 -0300

    Alteração da conexão com o banco de dados

--- public/db_config.js
+++ public/db_config.js
@@ -1,13 +1,21 @@
-// db_config.js
 require('dotenv').config();
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  connectionLimit: 10 // Define um limite para o número de conexões
+  connectionLimit: 10
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  } else {
+    console.log('Conectado ao banco de dados');
+    connection.release();
+  }
 });
 
 module.exports = pool;
